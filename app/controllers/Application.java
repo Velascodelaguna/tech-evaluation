fb87ef55971641494ce9f982c1006546b90476dc
James Velasco <jvelasco@sfu.ca>
2014-06-07 23:21:25 -0700

    Correcting group number

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -12,7 +12,7 @@ public class Application extends Controller {
     public static Result index() {
     	List<String> names = Arrays.asList("James", "Rob", "Anastasia", "Maria", "Allan", "Ravneet", "Harpreet");
     	Collections.sort(names);
-        return ok(index.render("Welcome to the website of group 8", names));
+        return ok(index.render("Welcome to the website of group 14", names));
     }
 
 }
