c03ec167318193aa4ea23ef98aafdbe339ed63ac
James Velasco <jvelasco@sfu.ca>
2014-06-15 23:13:35 -0700

    -set correct xpath to get headings -missing closing square bracket in xpath info -add method for getting attribute values only -declare collections as final

--- app/controllers/GetPresentationContent.java
+++ app/controllers/GetPresentationContent.java
@@ -19,52 +19,57 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 public class GetPresentationContent {
-	
-	private final static String xpathHeadings = "/PlayContent/Heading/text()";
-	private final static String xpathInfo = "/PlayContent/Heading[@name=\"%s\"/info/text()";
-	
+
+    private final static String xpathHeadings = "/PlayContent/Heading/@name";
+    private final static String xpathInfo = "/PlayContent/Heading[@name=\"%s\"]/Info/text()";
+    private final static String quote = "\"";
+    
     public static Map<String, List<String>> getContent(final String path){
-	
-    	Map<String, List<String>> content = new LinkedHashMap<String, List<String>>();
-		List<String> headingList = new ArrayList<>();
-		
-		try {
-			
-			Document doc = DocumentBuilderFactory
-				       .newInstance()
-			               .newDocumentBuilder()
-			               .parse(new File(path));
-			
-			XPath xpath = XPathFactory.newInstance().newXPath();
-			NodeList headings = (NodeList) xpath.evaluate(xpathHeadings, doc, XPathConstants.NODESET);
-			
-			for (int i = 0; i < headings.getLength(); i++) {
-				headingList.add( getTextOnly(headings.item(i).toString() ));
-			}
-			
-			for (String heading: headingList) {
-				NodeList info = (NodeList) xpath.evaluate(String.format(xpathInfo, heading), doc, XPathConstants.NODESET);
-				List<String> infoList = new ArrayList<String>();
-				
-				for (int i = 0; i < info.getLength(); i++) {
-					infoList.add(getTextOnly(info.item(i).toString()));
-				}
-				
-				content.put(heading, infoList);
-			}
-			
-	
-		} catch (SAXException | IOException | XPathExpressionException | ParserConfigurationException e) {
-			e.printStackTrace();
-		}
-		
-		return content;
-	
+
+        final Map<String, List<String>> content = new LinkedHashMap<String, List<String>>();
+        final List<String> headingList = new ArrayList<>();
+
+        try {
+
+            final Document doc = DocumentBuilderFactory
+                    .newInstance()
+                    .newDocumentBuilder()
+                    .parse(new File(path));
+
+            XPath xpath = XPathFactory.newInstance().newXPath();
+            NodeList headings = (NodeList) xpath.evaluate(xpathHeadings, doc, XPathConstants.NODESET);
+
+            for (int i = 0; i < headings.getLength(); i++) {
+                headingList.add( getAttributeValue(headings.item(i).toString() ));
+            }
+
+            for (String heading: headingList) {
+                NodeList info = (NodeList) xpath.evaluate(String.format(xpathInfo, heading), doc, XPathConstants.NODESET);
+                List<String> infoList = new ArrayList<String>();
+
+                for (int i = 0; i < info.getLength(); i++) {
+                    infoList.add(getTextOnly(info.item(i).toString()));
+                }
+
+                content.put(heading, infoList);
+            }
+        } catch (SAXException | IOException | XPathExpressionException | ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+
+        return content;
+
+    }
+
+    private static String getTextOnly(final String text) {
+        int colonIndex = text.indexOf(":") + 1;
+        int lastSqBracketIndex = text.lastIndexOf("]");
+        return text.substring(colonIndex, lastSqBracketIndex).trim();
     }
     
-    private static String getTextOnly(final String text) {
-		int colonIndex = text.indexOf(":") + 1;
-		int lastIndex = text.lastIndexOf("]");
-		return text.substring(colonIndex, lastIndex).trim();
+    private static String getAttributeValue(final String keyValPair) {
+        int firstQuoteIndex = keyValPair.indexOf(quote);
+        int lastQuoteIndex = keyValPair.lastIndexOf(quote);
+        return keyValPair.substring(firstQuoteIndex+1, lastQuoteIndex).trim();
     }
 }
